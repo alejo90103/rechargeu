3a36943e9479a090ba10ccddd8f279eb048802dc
Alejandro Herrera <39777010+alejo90103@users.noreply.github.com>
2019-12-08 04:48:15 +0100

    Add files via upload

---
+++ vue/src/env.js
@@ -0,0 +1,14 @@
+/**
+ * @Author: Codeals
+ * @Date:   05-08-2019
+ * @Email:  ian@codeals.es
+ * @Last modified by:   Codeals
+ * @Last modified time: 21-11-2019
+ * @Copyright: Codeals
+ */
+
+export const clientId = '2'
+export const clientSecret = 'XYXHbI23XNN7ie1t4gyp9Clq09lSCE34dVWiRua8'
+export const clientGoogle = '872568307141-0v2spdrn7hjv2oe61mo0k2djuq0s1f1q.apps.googleusercontent.com'
+export const clientFacebook = '2604554419776526'
+// export const pusherSecret = '6602e248a34b53237216'
